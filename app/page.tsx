262e2be77fc1e2d27aa20975ad6bbf551cf4eecf
shivamRamani <shivamramani40@gmail.com>
2024-02-07 21:22:28 +0530

    feat: project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col bg-background">
+            <div className="my-10 mx-auto max-w-6xl w-full h-full bg-primary dark rounded-2xl p-20 text-background ">
+                <h1>SHIVAM</h1>
+                <h1>SHIVAM</h1>
+                <h1>SHIVAM</h1>
+            </div>
+        </main>
+    );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                text: "var(--text)",
+                background: "var(--background)",
+                primary: "var(--primary)",
+                secondary: "var(--secondary)",
+                accent: "var(--accent)",
+            },
+        },
+    },
+
+    plugins: [],
+};
+export default config;
